2b877a7e96da592a19e64f005561c4b4890eab79
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-06-19 13:24:47 +0600

    back

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -11,8 +11,11 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .background(.red)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
